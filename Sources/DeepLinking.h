bb3e145faf84a12f32d65615a4c9d326d0e0856e
emile <emile@blueantcorp.com>
2023-04-15 00:51:42 +0400

    xcoden
    Signed-off-by: emile <emile@blueantcorp.com>

---
+++ Sources/DeepLinking.h
@@ -0,0 +1,18 @@
+//
+//  DeepLinking.h
+//  DeepLinking
+//
+//  Created by emile on 15/04/2023.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for DeepLinking.
+FOUNDATION_EXPORT double DeepLinkingVersionNumber;
+
+//! Project version string for DeepLinking.
+FOUNDATION_EXPORT const unsigned char DeepLinkingVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DeepLinking/PublicHeader.h>
+
+
